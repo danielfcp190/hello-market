1c5f1f89215f30cc36cee54664731c14fc0b9f3f
Daniel Ferreira da Costa Pinto <91293389+danielfcp190@users.noreply.github.com>
2022-05-12 16:48:14 -0300

    starting a new project with react-query

---
+++ models/Product.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  id: { type: String },
+  name: { type: String },
+});
+
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
+export default Product;
